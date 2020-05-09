e4996a7c9542baaa5e3b4888d516d4d1ff66be10
Whynot-code <62120468+Whynot-code@users.noreply.github.com>
2020-05-09 17:41:41 +0200

    Add files via upload
    PARCEL installation is required for proper operation.
    https://parceljs.org/
    Add 'parcel index.html' in command line interface to turn it on.

---
+++ weryfikacja_karty/index.js
@@ -0,0 +1,18 @@
+import checkCardNumber from './checkCardNumber';
+
+console.log(checkCardNumber(378282246310005))
+console.log(checkCardNumber(371449635398431))
+console.log(checkCardNumber(378734493671000))
+console.log(checkCardNumber(30569309025904))
+console.log(checkCardNumber(6011111111111117))
+console.log(checkCardNumber(6011000990139424))
+console.log(checkCardNumber(3530111333300000))
+console.log(checkCardNumber(3566002020360505))
+console.log(checkCardNumber(2221000000000009))
+console.log(checkCardNumber(2223000048400011))
+console.log(checkCardNumber(2223016768739313))
+console.log(checkCardNumber(5555555555554444))
+console.log(checkCardNumber(5105105105105100))
+console.log(checkCardNumber(4111111111111111))
+console.log(checkCardNumber(4012888888881881))
+console.log(checkCardNumber(4222222222222))
